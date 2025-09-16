fcf5f309a8d1a1655bb317f238db76fb4f2bb50a
Vincent C <vinch.cm@gmail.com>
2025-09-16 13:52:19 +0200

    TodoList component completed with buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import TodoList from "./components/TodoList";
 import "./CSS/App.css";
 
 function App() {
   return (
     <>
-      <p>Hello</p>
+      <TodoList />
     </>
   );
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import TodoTypes from "../todo";
 import TodoService from "../TodoService";
+import { FaEdit, FaCheck } from "react-icons/fa";
+import { GiCancel } from "react-icons/gi";
+import { RiDeleteBin5Fill } from "react-icons/ri";
 
 const TodoList = () => {
   const [todos, setTodos] = useState<TodoTypes[]>(TodoService.getTodos());
@@ -13,7 +16,7 @@ const TodoList = () => {
     setEditedTodoText(text);
   };
 
-  const handleEditcancel = () => {
+  const handleEditCancel = () => {
     setEditedTodoId(null);
     setEditedTodoText("");
   };
@@ -38,7 +41,42 @@ const TodoList = () => {
     setTodos((prevTodo) => prevTodo.filter((t) => t.id !== id));
   };
 
-  return <></>;
+  return (
+    <div className="todoContainer">
+      <div></div>
+
+      {todos.map((todo) => (
+        <div className="items" key={todo.id}>
+          {editingTodoId == todo.id ? (
+            <div className="editedText">
+              <input
+                type="text"
+                value={editedTodoText}
+                onChange={(e) => setEditedTodoText(e.target.value)}
+                autoFocus={true}
+              />
+              <button onClick={() => handleEditSave(todo.id)}>
+                <FaCheck />
+              </button>
+              <button className="cancelBtn" onClick={() => handleEditCancel()}>
+                <GiCancel />
+              </button>
+            </div>
+          ) : (
+            <div className="editBtn">
+              <span>{todo.text}</span>
+              <button onClick={() => handleEditStart(todo.id, todo.text)}>
+                <FaEdit />
+              </button>
+            </div>
+          )}
+          <button onClick={() => handleDelete(todo.id)}>
+            <RiDeleteBin5Fill />
+          </button>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default TodoList;
