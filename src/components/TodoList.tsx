c41c7edf9f2a28d0e4a0ea5102afc6487a0ee742
Vincent C <vinch.cm@gmail.com>
2025-09-16 13:28:23 +0200

    front functions created in TodoList component

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -5,7 +5,38 @@ import TodoService from "../TodoService";
 const TodoList = () => {
   const [todos, setTodos] = useState<TodoTypes[]>(TodoService.getTodos());
   const [editingTodoId, setEditedTodoId] = useState<number | null>(null);
-  const [editedTodoText, setEditedTodoText] = useState<string | null>("");
+  const [editedTodoText, setEditedTodoText] = useState<string>("");
+
+  // functions for handling Edit actions
+  const handleEditStart = (id: number, text: string) => {
+    setEditedTodoId(id);
+    setEditedTodoText(text);
+  };
+
+  const handleEditcancel = () => {
+    setEditedTodoId(null);
+    setEditedTodoText("");
+  };
+
+  const handleEditSave = (id: number) => {
+    if (editedTodoText.trim() !== "") {
+      const updateTodo = TodoService.updateTodo({
+        id,
+        text: editedTodoText,
+        completed: false,
+      });
+      setTodos((prevTodos) =>
+        prevTodos.map((todo) => (todo.id === id ? updateTodo : todo))
+      );
+      setEditedTodoId(null);
+      setEditedTodoText("");
+    }
+  };
+
+  const handleDelete = (id: number) => {
+    TodoService.deleteTodo(id);
+    setTodos((prevTodo) => prevTodo.filter((t) => t.id !== id));
+  };
 
   return <></>;
 };
