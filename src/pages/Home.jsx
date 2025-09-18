4f3974ab685d8dabebd024298a98e15a625bfc6a
Vincent C <vinch.cm@gmail.com>
2025-09-18 15:30:19 +0200

    Home with all data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,40 @@
+import { useState } from "react";
+import Search from "../components/utils/Search";
+import { COUNTRIES } from "../data/data";
+import { CLUBS } from "../data/data";
+import { PLAYERS } from "../data/data";
+import CountryCard from "../components/CountryCard";
+import ClubCard from "../components/ClubCard";
+import PlayerCard from "../components/PlayerCard";
+
 export default function Home() {
+  const [search, setSearch] = useState("");
   return (
     <>
-      <p>Hello</p>
+      <Search search={search} handleSearchUpdate={setSearch} />
+      <div className="flex flex-wrap justify-center items-center">
+        {COUNTRIES.filter((country) =>
+          country.name.toLowerCase().includes(search.toLowerCase())
+        )
+          .sort((a, b) => b.id - a.id)
+          .map((country, index) => (
+            <CountryCard key={index} country={country} />
+          ))}
+        {CLUBS.filter((club) =>
+          club.name.toLowerCase().includes(search.toLowerCase())
+        )
+          .sort((a, b) => b.id - a.id)
+          .map((club, index) => (
+            <ClubCard key={index} club={club} />
+          ))}
+        {PLAYERS.filter((player) =>
+          player.full_name.toLowerCase().includes(search.toLowerCase())
+        )
+          .sort((a, b) => b.id - a.id)
+          .map((player, index) => (
+            <PlayerCard key={index} player={player} />
+          ))}
+      </div>
     </>
   );
 }
